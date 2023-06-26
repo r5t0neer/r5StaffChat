54af623d4cc6d71c1831097bd3ae4accac6c02f6
Rentix57 Rentix57 <r5t0neer.dev@gmail.com>
2023-06-26 23:46:05 +0200

    setup

---
+++ src/main/java/me/r5t0neer/mcp/sc/R5StaffChat.java
@@ -0,0 +1,12 @@
+package me.r5t0neer.mcp.sc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class R5StaffChat extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+}
